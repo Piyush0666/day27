1936e4c6bdb208dcb75cdb17c80e5c328ab9ed8a
your_github_username <your_github_email>
2022-03-17 17:55:54 +0530

    Files_using_FileIO

---
+++ src/main/java/com/bridgelabz/AddresBookIO/Day27/AddressBook.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.AddresBookIO.Day27;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddressBook {
+    public static Map<String, Contact> book = new HashMap<>();
+
+}
---
+++ src/test/java/com/bridgelabz/AddresBookIO/Day27/AddressBookMainTest.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.AddresBookIO.Day27;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressBookMainTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
